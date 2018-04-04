cc2c0573ac0f55cc0c5ad622c4a687757ff22f7e
Lee Khai <leekhai18@gmail.com>
2018-04-04 23:52:03 +0700

    binding data to home page

--- SOH/SOH.Web/Pages/Index.cshtml.cs
+++ SOH/SOH.Web/Pages/Index.cshtml.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SOH.Web.Data;
 
 namespace SOH.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _context;
 
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Data.Event> Events { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Events = await _context.Events.Include(e => e.Images).ToListAsync();
         }
     }
 }
