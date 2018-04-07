649f4f098ad673a5baf5c07836fd1b95808e0a12
Lam Le <15520405@gm.uit.edu.vn>
2018-04-07 23:51:20 +0700

    Added sql script to intialize database.

---
+++ SOH/migration.sql
@@ -0,0 +1,365 @@
+﻿IF OBJECT_ID(N'__EFMigrationsHistory') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [AspNetRoles] (
+        [Id] uniqueidentifier NOT NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [Name] nvarchar(256) NULL,
+        [NormalizedName] nvarchar(256) NULL,
+        CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [AspNetUsers] (
+        [Id] uniqueidentifier NOT NULL,
+        [AccessFailedCount] int NOT NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [Email] nvarchar(256) NULL,
+        [EmailConfirmed] bit NOT NULL,
+        [LockoutEnabled] bit NOT NULL,
+        [LockoutEnd] datetimeoffset NULL,
+        [NormalizedEmail] nvarchar(256) NULL,
+        [NormalizedUserName] nvarchar(256) NULL,
+        [PasswordHash] nvarchar(max) NULL,
+        [PhoneNumber] nvarchar(max) NULL,
+        [PhoneNumberConfirmed] bit NOT NULL,
+        [SecurityStamp] nvarchar(max) NULL,
+        [TwoFactorEnabled] bit NOT NULL,
+        [UserName] nvarchar(256) NULL,
+        CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [Events] (
+        [Id] uniqueidentifier NOT NULL,
+        [Description] nvarchar(max) NULL,
+        [EndingDate] datetime2 NULL,
+        [StartingDate] datetime2 NOT NULL,
+        [Title] nvarchar(max) NULL,
+        CONSTRAINT [PK_Events] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [Products] (
+        [Id] uniqueidentifier NOT NULL,
+        [Price] bigint NOT NULL,
+        [StockQuantity] int NOT NULL,
+        [Title] nvarchar(max) NULL,
+        CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [AspNetRoleClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        [RoleId] uniqueidentifier NOT NULL,
+        CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [AspNetUserClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        [UserId] uniqueidentifier NOT NULL,
+        CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [AspNetUserLogins] (
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [ProviderKey] nvarchar(450) NOT NULL,
+        [ProviderDisplayName] nvarchar(max) NULL,
+        [UserId] uniqueidentifier NOT NULL,
+        CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+        CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [AspNetUserRoles] (
+        [UserId] uniqueidentifier NOT NULL,
+        [RoleId] uniqueidentifier NOT NULL,
+        CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+        CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [AspNetUserTokens] (
+        [UserId] uniqueidentifier NOT NULL,
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [Name] nvarchar(450) NOT NULL,
+        [Value] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name]),
+        CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [Images] (
+        [Id] uniqueidentifier NOT NULL,
+        [EventId] uniqueidentifier NULL,
+        [ProductId] uniqueidentifier NULL,
+        [URI] nvarchar(max) NULL,
+        CONSTRAINT [PK_Images] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Images_Events_EventId] FOREIGN KEY ([EventId]) REFERENCES [Events] ([Id]) ON DELETE NO ACTION,
+        CONSTRAINT [FK_Images_Products_ProductId] FOREIGN KEY ([ProductId]) REFERENCES [Products] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE TABLE [Posts] (
+        [Id] uniqueidentifier NOT NULL,
+        [ApplicationUserId] uniqueidentifier NOT NULL,
+        [Content] nvarchar(max) NULL,
+        [Created] datetime2 NOT NULL,
+        [ImageId] uniqueidentifier NULL,
+        [LastAccessed] datetime2 NOT NULL,
+        [Title] nvarchar(max) NULL,
+        CONSTRAINT [PK_Posts] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_Posts_AspNetUsers_ApplicationUserId] FOREIGN KEY ([ApplicationUserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_Posts_Images_ImageId] FOREIGN KEY ([ImageId]) REFERENCES [Images] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_Images_EventId] ON [Images] ([EventId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_Images_ProductId] ON [Images] ([ProductId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_Posts_ApplicationUserId] ON [Posts] ([ApplicationUserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    CREATE INDEX [IX_Posts_ImageId] ON [Posts] ([ImageId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034253_RecreateTables')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180315034253_RecreateTables', N'2.0.1-rtm-125');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180315034923_ChangeToIdentityRole')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180315034923_ChangeToIdentityRole', N'2.0.1-rtm-125');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405072448_UpdateEventTable')
+BEGIN
+    ALTER TABLE [Events] ADD [AgeInformation] nvarchar(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405072448_UpdateEventTable')
+BEGIN
+    ALTER TABLE [Events] ADD [CategoryInformation] nvarchar(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405072448_UpdateEventTable')
+BEGIN
+    ALTER TABLE [Events] ADD [TicketInformation] nvarchar(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405072448_UpdateEventTable')
+BEGIN
+    ALTER TABLE [Events] ADD [TimeInformation] nvarchar(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405072448_UpdateEventTable')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180405072448_UpdateEventTable', N'2.0.1-rtm-125');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405082846_UpdateEventTable02')
+BEGIN
+    DROP INDEX [IX_Posts_ImageId] ON [Posts];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405082846_UpdateEventTable02')
+BEGIN
+    CREATE UNIQUE INDEX [IX_Posts_ImageId] ON [Posts] ([ImageId]) WHERE [ImageId] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180405082846_UpdateEventTable02')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180405082846_UpdateEventTable02', N'2.0.1-rtm-125');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180406073625_AddMessageTable')
+BEGIN
+    CREATE TABLE [Messages] (
+        [Id] uniqueidentifier NOT NULL,
+        [Content] nvarchar(max) NULL,
+        [Email] nvarchar(max) NULL,
+        [Name] nvarchar(max) NULL,
+        [Subject] nvarchar(max) NULL,
+        CONSTRAINT [PK_Messages] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180406073625_AddMessageTable')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180406073625_AddMessageTable', N'2.0.1-rtm-125');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180406124222_AddDescriptionToProductTable')
+BEGIN
+    ALTER TABLE [Products] ADD [Description] nvarchar(max) NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20180406124222_AddDescriptionToProductTable')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20180406124222_AddDescriptionToProductTable', N'2.0.1-rtm-125');
+END;
+
+GO
+
